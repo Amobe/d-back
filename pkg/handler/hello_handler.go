4cee8e1ddc39ae6d6bc4b6e7de5adbaf7510be72
Lex Liu <amobe.momoko@gmail.com>
2020-11-30 01:04:05 +0800

    Implement hello handler to call a real service
    Hello handler handles the http request and
    verify the request data.
    Calling the ip limiter service for deciding the request is accepted or not.

--- pkg/handler/hello_handler.go
+++ pkg/handler/hello_handler.go
@@ -2,21 +2,41 @@ package handler
 
 import (
 	"fmt"
+	"net/http"
+
+	"github.com/amobe/d-back/pkg/entity"
+	"github.com/amobe/d-back/pkg/service/iplimiter"
 
 	"github.com/gin-gonic/gin"
 )
 
 // Hello represents a handler
 type Hello struct {
+	ils iplimiter.Service
 }
 
 // NewHelloHandler creates a hello handler instance.
-func NewHelloHandler() Hello {
-	return Hello{}
+func NewHelloHandler(ils iplimiter.Service) Hello {
+	return Hello{
+		ils: ils,
+	}
 }
 
 // Handle handles the hello request.
 // The remote address of the client is recorded.
 func (h *Hello) Handle(ctx *gin.Context) {
-	fmt.Println(ctx.Request.RemoteAddr)
+	ipAddress, err := entity.NewIPAddress(ctx.Request.RemoteAddr)
+	if err != nil {
+		fmt.Printf("[Err] HelloHandle | new ip address: %s\n", err)
+		ctx.JSON(http.StatusBadRequest, err)
+		return
+	}
+
+	token, err := h.ils.AcceptRequest(ipAddress)
+	if err != nil {
+		fmt.Printf("[Err] HelloHandle | accept request: %s\n", err)
+		ctx.JSON(http.StatusServiceUnavailable, err)
+		return
+	}
+	fmt.Println(token)
 }
